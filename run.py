b9027558a6ac3fbce4f7e146b0156f26ce2a34c9
Hugh Keenan <hugh.keenan114@gmail.com>
2024-06-05 21:08:56 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -53,6 +53,8 @@ def validate_data(values):
         return False        
     return True
 
+#below commented out, see update worksheet for example on refactoring
+'''
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -70,6 +72,17 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print('Surplus worksheet updated successfully.\n')    
+'''
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    update the relevant worksheet with the data provided
+    """
+    print (f'Updating {worksheet} worksheet...')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f'{worksheet} worksheet updated successfully.')
 
 def calculate_surplus_data(sales_row):
     """
@@ -95,9 +108,9 @@ def main():
     """
     data = get_sales_data()    
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
 print('Welcome to Love Sandwiches data Automation!')
 main()
